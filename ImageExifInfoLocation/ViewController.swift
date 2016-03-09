625e61374e015024ed2667a335e934a10498e4df
King Ying <ykyingbox@gmail.com>
2016-03-09 18:03:02 +0800

    Initial Commit

---
+++ ImageExifInfoLocation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageExifInfoLocation
+//
+//  Created by King on 9/3/2016.
+//  Copyright © 2016 YING YIK KING. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
